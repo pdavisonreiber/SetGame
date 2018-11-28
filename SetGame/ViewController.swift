37176e4d8960620712d63eaaae1900a14e25f3a9
Peter Davison-Reiber <pdavisonreiber@gmail.com>
2018-11-28 20:40:54 +0000

    Continuing to work on UI

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLabels()
+        setLabels(.two, .three, .three, .two)
     }
 
     @IBOutlet var cardButtons: [UIButton]!
@@ -23,19 +23,73 @@ class ViewController: UIViewController {
         }
     }
     
-    func setLabels() {
+    func colorAndShadingForAttributes(ColorAttribute: Card.Attribute, ShadingAttribute: Card.Attribute) -> [NSAttributedString.Key: Any] {
+        
+        let color: UIColor
+        
+        switch ColorAttribute {
+        case .one: color = .red
+        case .two: color = .blue
+        case .three: color = .green
+        }
+        
+        switch ShadingAttribute {
+        case .one: return [
+            .foregroundColor: color
+            ]
+        case .two: return [
+            .foregroundColor: color.withAlphaComponent(CGFloat(0.2))
+            ]
+        case .three: return [
+            .strokeWidth: 10.0
+            ]
+        }
+        
+    }
+    
+    func characterForAttribute(_ attribute: Card.Attribute) -> Character {
+        switch attribute {
+        case .one: return "■"
+        case .two: return "▲"
+        case .three: return "●"
+        }
+    }
+    
+    func numberForAttribute(_ attribute: Card.Attribute) -> Int {
+        switch attribute {
+        case .one: return 1
+        case .two: return 2
+        case .three: return 3
+        }
+    }
+    
+    func repeatCharacterWithLineBreaks(character: Character, repeats: Int) -> String {
+        var string = ""
+        for _ in 1..<repeats{
+            string += String(character)
+            string += "\n"
+        }
+        string += String(character)
+        return string
+    }
+    
+    func setLabels(_ attribute1: Card.Attribute, _ attribute2: Card.Attribute, _ attribute3: Card.Attribute, _ attribute4: Card.Attribute) {
         for button in cardButtons {
-            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+            button.layer.cornerRadius = 8.0
+            
+            let attributes = colorAndShadingForAttributes(ColorAttribute: attribute1, ShadingAttribute: attribute2)
+            let cardString = repeatCharacterWithLineBreaks(character: characterForAttribute(attribute3), repeats: numberForAttribute(attribute4))
+            let cardAttributedString = NSAttributedString(string: cardString, attributes: attributes)
+            
+            button.setAttributedTitle(cardAttributedString, for: UIControl.State.normal)
+            
+            
         }
     }
     
     func titleForCard(card: Card) {
-        let colorsDictionary: [Card.Attribute: UIColor] = [.one: .red, .two: .blue, .three: .green]
-        let shadingsDictionary 
-        switch card.attributes[0] {
-        case .one:
-            let
-        }
+        
     }
     
     
