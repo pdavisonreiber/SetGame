ef68181628996ebac261ed8733bff16956f6f334
Peter Davison-Reiber <pdavisonreiber@gmail.com>
2018-11-30 07:01:16 +0000

    Experimenting with subclassing UIButton

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -15,14 +15,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setInitialCardButtons()
+        cardButtons = cardButtons.shuffled()
+        assignInitialCardsToCardButtons()
     }
 
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet var cardButtons: [CardButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         if cardButtonAssignments.keys.contains(sender) {
-            //3selectButton(sender)
+            //selectButton(sender)
         }
     }
     
@@ -35,85 +36,15 @@ class ViewController: UIViewController {
     
     @IBAction func touchNewGameButton() {
         scoreLabel.text = "Score: 0"
-        cardButtonAssignments = [:]
         game.shuffleCards()
-        setInitialCardButtons()
     }
     
-    
-//    func selectButton(_ button: UIButton) {
-//        if button.strok
-//    }
-    
-    func colorAndShadingForAttributes(ColorAttribute: Card.Attribute, ShadingAttribute: Card.Attribute) -> [NSAttributedString.Key: Any] {
-        
-        let color: UIColor
-        
-        switch ColorAttribute {
-        case .one: color = .red
-        case .two: color = .blue
-        case .three: color = .green
-        }
-        
-        switch ShadingAttribute {
-        case .one: return [
-            .foregroundColor: color
-            ]
-        case .two: return [
-            .foregroundColor: color.withAlphaComponent(CGFloat(0.2))
-            ]
-        case .three: return [
-            .strokeWidth: 10.0
-            ]
-        }
-        
-    }
-    
-    func characterForAttribute(_ attribute: Card.Attribute) -> Character {
-        switch attribute {
-        case .one: return "■"
-        case .two: return "▲"
-        case .three: return "●"
-        }
-    }
-    
-    func numberForAttribute(_ attribute: Card.Attribute) -> Int {
-        switch attribute {
-        case .one: return 1
-        case .two: return 2
-        case .three: return 3
-        }
-    }
-    
-    func repeatCharacterWithLineBreaks(character: Character, repeats: Int) -> String {
-        var string = ""
-        for _ in 1..<repeats{
-            string += String(character)
-            string += "\n"
-        }
-        string += String(character)
-        return string
-    }
-    
-    func assignCardToButton(card: Card, button: UIButton) {
-        button.backgroundColor = UIColor.white
-        button.layer.cornerRadius = 8.0
-        
-        let attributes = colorAndShadingForAttributes(ColorAttribute: card.attributes[0], ShadingAttribute: card.attributes[1])
-        let cardString = repeatCharacterWithLineBreaks(character: characterForAttribute(card.attributes[2]), repeats: numberForAttribute(card.attributes[3]))
-        let cardAttributedString = NSAttributedString(string: cardString, attributes: attributes)
-        
-        button.setAttributedTitle(cardAttributedString, for: UIControl.State.normal)
-        
-        cardButtonAssignments[button] = card
-    }
-    
-    func setInitialCardButtons() {
-        let shuffledCardButtons = cardButtons.shuffled()
+    func assignInitialCardsToCardButtons() {
         for index in 1...12 {
             assignCardToButton(card: game.cards[index], button: shuffledCardButtons[index])
         }
     }
+    
     
 }
 
